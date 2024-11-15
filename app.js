5c78b5f44981efca79a838375f7f33bfd67ac22a
dannyCastro18 <danllelacrespactr018@gmail.com>
2024-11-15 18:13:02 -0500

    Evitar usuario clonado

---
+++ app.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors'); //Para permitir solicitudes desde otro puerto
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const app = express();
+app.use(cors()); 
+app.use(express.json()); //para parsear JSON
+
+const PORT = process.env.PORT||3000;
+const JWT_SECRET = process.env.JWT_SECRET; 
+
+const users = [
+    {id:1, name:"Loly Villamizar", email: "lolyvillamizar@gmail.com", password: "123454321"}, 
+    {id:2, name:"Ivan Villamil", email: "ivanvillamil12@gmail.com", password: "asdf"},
+    {id:3, name:"Tamara Franco", email: "tamara098@gmail.com", password: "qwer"},
+    {id:4, name:"Mario Vargas Carvajal", email: "mariovargascarvajar@gmail.com", password: "0987"}
+]; //simula una base de datos
+
+app.post('/register',async(req,res)=>{
+    const {name, email, password} = req.body;
+    
+    //Encriptar contraseña
+    const hashedPassword = await bcrypt.hash(password,10);
+    const newUser = {name, email, password: hashedPassword};
+    users.push(newUser);
+    
+    console.log(users)
+    res.status(201).json({message: 'Usuario registrado exitosamente'});
+
+});
+
+app.post('/login',async(req,res)=>{
+    const {email,password} = req.body;
+    const user = users.find(user => user.email === email);
+
+    if(!user || !(await bcrypt.compare(password, user.password))){
+        return res.status(401).json({message: 'Credenciales incorrectas'});
+    }
+
+    //Genera token JWT
+    const token = jwt.sign({ email: user.email}, process.env.JWT_SECRET,{
+        expiresIn:'1h'});
+    console.log("Haz iniciado sesión", token);
+    res.json({token});
+}); 
+
+const authenticateToken = (req, res, next) =>{
+    const authHeader = req.headers['authorization'];
+    const token = authHeader&&authHeader.split(' ')[1];
+
+    if(!token) return res.status(401).json({message: 'token requerido'});
+
+    jwt.verify(token,JWT_SECRET,(err,user)=>{
+        if(err) return res.status(403).json({message: 'Token inválido'});
+        req.user = user;
+        next();
+    });
+};
+
+app.get('/protected', authenticateToken,(req, res)=>{
+    res.json({message: 'Acceso autorizado', user: req.user});
+});
+
+app.listen(PORT,()=>{
+    console.log(`Servidor en funcionamiento en http://localhost:${PORT}`);
+});
+
+
